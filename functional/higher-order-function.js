0b1bdfaf30a236537f40628d9e19311e6d891a75
Luis Felipe Veronezi Bernardo <luisfelipevbernardo@gmail.com>
2024-11-22 08:28:59 -0300

    aulas

---
+++ functional/higher-order-function.js
@@ -0,0 +1,30 @@
+/*
+ * Functions that operate on other functions,
+ * either by taking them as arguments or by
+ * returning them, are called higher-order functions.
+ */
+
+function run(fn) {
+    fn()
+}
+
+function sayHello() {
+    console.log('Hello!!')
+}
+
+run(sayHello)
+
+
+function sayHello() {
+    console.log('Hello!!!')
+}
+
+run(sayHello)
+
+run(function() {
+    console.log('run!!!')
+})
+
+const result = run(Math.random)
+console.log(result)
+
